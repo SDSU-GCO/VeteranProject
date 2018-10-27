666b7b726615408139550bc90fae8b312e3f5c49
Alexander Evans <alexander.evans@jacks.sdstate.edu>
2018-10-27 06:42:52 -0500

    Wrote the Chunk Class

---
+++ VeteranProject/Assets/Scripts/Chunk.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is a chunk 
+/// </summary>
+public class Chunk : MonoBehaviour {
+
+    public static Bounds? ChunkSize = null;
+    Sprite sprite = null;
+
+
+
+    Placeable currentPlaceable = null;
+    
+
+    public Placeable Placeable
+    {
+        get
+        {
+            return currentPlaceable;
+        }
+        set
+        {
+            if(currentPlaceable!=null)
+            {
+                Destroy(currentPlaceable.gameObject);
+            }
+
+            currentPlaceable = Instantiate(value, transform);
+        }
+    }
+
+    private void OnValidate()
+    {
+        Sprite temp = GetComponent<Sprite>();
+        if (sprite != null && sprite != temp)
+        {
+            sprite = temp;
+            Debug.Assert(sprite != null, "Error: Sprite in " + this + " is null!");
+            if (ChunkSize == null)
+            {
+                ChunkSize = sprite.bounds;
+            }
+            else if (ChunkSize != sprite.bounds)
+            {
+                Debug.LogError("Chunk Size Sprite Bounding Error in " + this + "!");
+            }
+        }
+    }
+
+
+
+}
